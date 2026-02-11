e71a2ea887cdade6e821e98ade53b5559595bae4
BlackSigkill <blacksigkill@gmail.com>
2026-02-11 14:38:12 +0100

    fix settings doesn't start

--- js/settings.js
+++ js/settings.js
@@ -13,6 +13,7 @@ import {
     smoothScrollingSettings,
     downloadQualitySettings,
     losslessContainerSettings,
+    serverDownloadSettings,
     coverArtSizeSettings,
     qualityBadgeSettings,
     trackDateSettings,
